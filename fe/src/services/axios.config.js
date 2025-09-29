eec1b7f73db724a55bcac5d87aa0c865c8fb90aa
Dinh <dinhp5432@gmail.com>
2025-09-29 21:49:28 +0700

    resolve conflict axios services

--- fe/src/services/axios.config.js
+++ fe/src/services/axios.config.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "/api",
+  baseURL: "/api",
 });
 instance.interceptors.response.use(function onFulfilled(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    if(response.data){
-        return response.data;
-    }
-    return response;
-  }, function onRejected(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    console.log(">>>check error", error);
-    if(error.response && error.response.data){
-        return error.response.data;
-    }
-    return Promise.reject(error);
-  });
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  if (response.data) {
+    return response.data;
+  }
+  return response;
+}, function onRejected(error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  console.log(">>>check error", error);
+  if (error.response && error.response.data) {
+    return error.response.data;
+  }
+  return Promise.reject(error);
+});
 export default instance;
--- fe/src/services/axios.services.js
+++ fe/src/services/axios.services.js
@@ -1,13 +1,21 @@
 import axios from "./axios.config";
-const registerAPI=(fullName, email, phone, address, password, confirmPassword)=>{
-    const data={
-        fullName: fullName, 
-        email: email, 
-        phone: phone, 
-        address: address, 
-        password: password, 
+const registerAPI = (fullName, email, phone, address, password, confirmPassword) => {
+    const data = {
+        fullName: fullName,
+        email: email,
+        phone: phone,
+        address: address,
+        password: password,
         confirmPassword: confirmPassword,
     }
     return axios.post("/auth/register", data);
 }
-export {registerAPI};
+const loginAPI = (email, password) => {
+    const data = {
+        email: email,
+        password: password
+    };
+    return axios.post("/auth/login", data);
+}
+
+export { registerAPI, loginAPI };
